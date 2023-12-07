c87ed47dba2bfc5287a80518ed9358160689094d
NekkidBear <jmking2009@live.com>
2023-12-07 13:57:58 -0600

    Adding gui and copy functionality. Also implemented a temp folder for recovery.

--- main.py
+++ main.py
@@ -23,8 +23,8 @@ os.makedirs(temp_dir, exist_ok=True)
 # function to set the backup directory
 def set_backup_dir():
     try:
-        root_window = Tk()
-        root_window.withdraw()  # Hide the main window
+        root = Tk()
+        root.withdraw()  # Hide the main window
         messagebox.showinfo('Prompt', 'Select the directory for comparison.')
         backup_directory = filedialog.askdirectory()  # Show the file dialog and get the selected directory
     except TclError:
@@ -40,7 +40,7 @@ def compare_zip_files(file1, file2):
 
 
 # function to check and delete duplicates
-def check_and_delete_duplicates(backup_directory, progress_variable, log):
+def check_and_delete_duplicates(backup_directory, progress_var, history_text):
     # get a list of all files in the downloads directory
     downloads_files = os.listdir(path_to_downloads_dir)
 
@@ -48,16 +48,16 @@ def check_and_delete_duplicates(backup_directory, progress_variable, log):
     backup_files = os.listdir(backup_directory)
 
     # iterate over all files in the downloads directory
-    for x, file_name in enumerate(downloads_files):
+    for i, filename in enumerate(downloads_files):
         # update the progress bar
-        progress_variable.set(x / len(downloads_files) * 100)
+        progress_var.set(i / len(downloads_files) * 100)
 
         # construct the full path to the file
-        file_path = os.path.join(path_to_downloads_dir, file_name)
-        backup_file_path = os.path.join(backup_directory, file_name)
+        file_path = os.path.join(path_to_downloads_dir, filename)
+        backup_file_path = os.path.join(backup_directory, filename)
 
         # if a file in the downloads directory exists in the backup directory
-        if file_name in backup_files:
+        if filename in backup_files:
             # if the file is a zip file, compare the contents
             if zipfile.is_zipfile(file_path) and zipfile.is_zipfile(backup_file_path):
                 if not compare_zip_files(file_path, backup_file_path):
@@ -65,30 +65,30 @@ def check_and_delete_duplicates(backup_directory, progress_variable, log):
 
             # ask for confirmation before moving the file to the temporary directory
             confirm = messagebox.askyesno('Confirmation',
-                                          f'I found {file_name} in {backup_directory}. Do you want to move the original to the temporary folder?')
+                                          f'I found {filename} in {backup_directory}. Do you want to move the original to the temporary folder?')
             if confirm:
                 try:
                     # try to move the file to the temporary directory
-                    shutil.move(file_path, os.path.join(temp_dir, file_name))
+                    shutil.move(file_path, os.path.join(temp_dir, filename))
                     messagebox.showinfo('Moved File', f'Moved {file_path} to the temporary directory')
-                    log.insert('end', f'Moved {file_path} to the temporary directory\n')
-                except ShutilError as move_error:
-                    print(f'Error occurred: {move_error}')
+                    history_text.insert('end', f'Moved {file_path} to the temporary directory\n')
+                except ShutilError as e:
+                    print(f'Error occurred: {e}')
         else:
             # if the file does not exist in the backup directory, ask if the user wants to copy it
             confirm = messagebox.askyesno('Confirmation',
-                                          f'{file_name} does not exist in {backup_directory}. Would you like to copy it?')
+                                          f'{filename} does not exist in {backup_directory}. Would you like to copy it?')
             if confirm:
                 try:
                     # try to copy the file
                     shutil.copy(file_path, backup_file_path)
                     messagebox.showinfo('Copied File', f'Copied {file_path} to {backup_file_path}')
-                    log.insert('end', f'Copied {file_path} to {backup_file_path}\n')
-                except ShutilError as move_error:
-                    print(f'Error occurred: {move_error}')
+                    history_text.insert('end', f'Copied {file_path} to {backup_file_path}\n')
+                except ShutilError as e:
+                    print(f'Error occurred: {e}')
 
     # set the progress bar to 100% when done
-    progress_variable.set(100)
+    progress_var.set(100)
 
 
 # usage
@@ -113,13 +113,13 @@ try:
 
     # create a button to delete the selected files
     def delete_files():
-        for checkbox_widget_del, var_state in checkboxes:
-            if var_state.get():
+        for checkbox_widget, var in checkboxes:
+            if var.get():
                 try:
-                    send2trash.send2trash(os.path.join(temp_dir, checkbox_widget_del.cget('text')))
-                    print(f'Moved {checkbox_widget_del.cget("text")} from the temporary directory to the Recycle Bin')
-                except Exception as del_error:
-                    print(f'Error occurred: {del_error}')
+                    send2trash.send2trash(os.path.join(temp_dir, checkbox_widget.cget('text')))
+                    print(f'Moved {checkbox_widget.cget("text")} from the temporary directory to the Recycle Bin')
+                except Exception as e:
+                    print(f'Error occurred: {e}')
         # refresh the listbox and checkboxes
         refresh()
 
@@ -129,14 +129,14 @@ try:
 
     # create a button to restore the selected files to their original location
     def restore_files():
-        for checkbox_widget_restore, var_restore_state in checkboxes:
-            if var_restore_state.get():
+        for checkbox_widget, var in checkboxes:
+            if var.get():
                 try:
-                    shutil.move(os.path.join(temp_dir, checkbox_widget_restore.cget('text')),
-                                os.path.join(path_to_downloads_dir, checkbox_widget_restore.cget('text')))
-                    print(f'Restored {checkbox_widget_restore.cget("text")} to the original location')
-                except Exception as rest_error:
-                    print(f'Error occurred: {rest_error}')
+                    shutil.move(os.path.join(temp_dir, checkbox_widget.cget('text')),
+                                os.path.join(path_to_downloads_dir, checkbox_widget.cget('text')))
+                    print(f'Restored {checkbox_widget.cget("text")} to the original location')
+                except Exception as e:
+                    print(f'Error occurred: {e}')
         # refresh the listbox and checkboxes
         refresh()
 
@@ -154,19 +154,19 @@ try:
         listbox.delete(0, 'end')
 
         # destroy the checkboxes
-        for checkbox_widget_refresh, var_state in checkboxes:
-            checkbox_widget_refresh.destroy()
+        for checkbox_widget, var in checkboxes:
+            checkbox_widget.destroy()
 
         # clear the list of checkboxes
         checkboxes.clear()
 
         # update the listbox and checkboxes
-        for file_name in os.listdir(temp_dir):
-            listbox.insert('end', file_name)
-            var_state = IntVar()
-            checkbox_widget_refresh = Checkbutton(root, text=file_name, variable=var_state)
-            checkbox_widget_refresh.pack()
-            checkboxes.append((checkbox_widget_refresh, var_state))
+        for filename in os.listdir(temp_dir):
+            listbox.insert('end', filename)
+            var = IntVar()
+            checkbox_widget = Checkbutton(root, text=filename, variable=var)
+            checkbox_widget.pack()
+            checkboxes.append((checkbox_widget, var))
 
 
     # create a history window
